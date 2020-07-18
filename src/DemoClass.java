ef87e020c4a36989f27c52cfc3846f53225fa052
Pranay Sharma <pranay.sharma_cs19@gla.ac.in>
2020-07-18 20:59:33 +0530

    created Scanner prog

--- src/DemoClass.java
+++ src/DemoClass.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class DemoClass {
+    public static void main(String[] args) {
+        Scanner object = new Scanner(System.in);
+        System.out.println("enter name:");
+        String name = object.nextLine();
+        System.out.println("Enter your age:");
+        byte age = object.nextByte();
+        System.out.println("Enter a 4 digit code:");
+        short code = object.nextShort();
+        System.out.println("enter your Cpi:");
+        double cpi = object.nextDouble();
+        System.out.println("Enter your mobile number");
+        long mobileNumber = object.nextLong();
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("code:" + code);
+        System.out.println("cpi:" + cpi);
+        System.out.println("Mobile number:" + mobileNumber);
+
+    }
 }
